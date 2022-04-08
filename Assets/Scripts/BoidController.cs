6ea7b00669e7a947362e5edc3eec3617b10c8430
Udayan Senapati <udayansenapati@gmail.com>
2022-04-08 10:03:42 -0400

    Working Boid System
    Includes multiple swarms, leaders, and target following

--- Assets/Scripts/BoidController.cs
+++ Assets/Scripts/BoidController.cs
@@ -6,20 +6,47 @@ public class BoidController : MonoBehaviour
     public int SwarmIndex { get; set; }
     [Header("Flocking Variables")]
     // Separation
-    [SerializeField] private float noClumpingRadius = 100f;
+    private float noClumpingRadius = 100f;
     // Alignment and Cohesion
-    [SerializeField] private float localAreaRadius = 10f;
-    [SerializeField] private float speed = 10f;
-    [SerializeField] private float steeringSpeed = 100f;
+    private float localAreaRadius = 10f;
+    // Speed
+    private float speed = 10f;
+    private float steeringSpeed = 100f;
+    [SerializeField]
     Vector3 steering = Vector3.zero;
 
+    [Header("Target")] 
+    [SerializeField] 
+    private GameObject target;
+    [SerializeField] 
+    private bool hasTarget;
+    
     Vector3 separationDirection = Vector3.zero;
-    int separationCount = 0;
+    int separationCount;
     Vector3 alignmentDirection = Vector3.zero;
-    int alignmentCount = 0;
+    int alignmentCount;
     Vector3 cohesionDirection = Vector3.zero;
-    int cohesionCount = 0;
+    int cohesionCount;
 
+    public void SetFlockingVariables(float noClumpingRadius, float localAreaRadius, float speed, float steeringSpeed)
+    {
+        this.noClumpingRadius = noClumpingRadius;
+        this.localAreaRadius = localAreaRadius;
+        this.speed = speed;
+        this.steeringSpeed = steeringSpeed;
+        hasTarget = false;
+    }
+
+    public void SetFlockingVariables(float noClumpingRadius, float localAreaRadius, float speed, float steeringSpeed, GameObject target)
+    {
+        this.noClumpingRadius = noClumpingRadius;
+        this.localAreaRadius = localAreaRadius;
+        this.speed = speed;
+        this.steeringSpeed = steeringSpeed;
+        this.target = target;
+        hasTarget = true;
+    }
+    
     public void SimulateMovement(List<BoidController> other, float time)
     {
         //default vars
@@ -95,8 +122,104 @@ public class BoidController : MonoBehaviour
             steering += (leaderBoid.transform.position - transform.position).normalized;
 
         //obstacle avoidance
-        RaycastHit hitInfo;
-        if (Physics.Raycast(transform.position, transform.forward, out hitInfo, localAreaRadius, LayerMask.GetMask("Default")))
+        if (Physics.Raycast(transform.position, transform.forward, out var hitInfo, localAreaRadius, LayerMask.GetMask("Default")))
+            steering = ((hitInfo.point + hitInfo.normal) - transform.position).normalized;
+
+        //apply steering
+        if (steering != Vector3.zero)
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.LookRotation(steering), steeringSpeed * time);
+
+        //move 
+        transform.position += transform.TransformDirection(new Vector3(0, 0, speed)) * time;
+    }
+    public void SimulateMovement(List<BoidController> other, float time, float separationWeight, float alignmentWeight, float cohesionWeight)
+    {
+        //default vars
+        steering = Vector3.zero;
+
+        separationDirection = Vector3.zero;
+        separationCount = 0;
+        alignmentDirection = Vector3.zero;
+        alignmentCount = 0;
+        cohesionDirection = Vector3.zero;
+        cohesionCount = 0;
+
+        Vector3 follow = Vector3.zero;
+        if (hasTarget)
+        {
+            follow = target.transform.position;
+        }
+        var leaderBoid = other[0];
+        var leaderAngle = 180f;
+
+        foreach (BoidController boid in other)
+        {
+            Transform boidTransfrom = boid.transform;
+            //skip self
+            if (boid == this)
+                continue;
+
+            var distance = Vector3.Distance(boid.transform.position, this.transform.position);
+
+            //identify local neighbour
+            if (distance < noClumpingRadius)
+            {
+                separationDirection += boid.transform.position - transform.position;
+                separationCount++;
+            }
+
+            //identify local neighbour
+            if (distance < localAreaRadius && boid.SwarmIndex == this.SwarmIndex)
+            {
+                alignmentDirection += boid.transform.forward;
+                alignmentCount++;
+
+                cohesionDirection += boidTransfrom.position - transform.position;
+                cohesionCount++;
+
+                //identify leader
+                var angle = Vector3.Angle(boidTransfrom.position - transform.position, transform.forward);
+                if (angle < leaderAngle && angle < 90f)
+                {
+                    leaderBoid = boid;
+                    leaderAngle = angle;
+                }
+            }
+        }
+
+        if (separationCount > 0)
+            separationDirection /= separationCount;
+
+        //flip
+        separationDirection = -separationDirection;
+
+        if (alignmentCount > 0)
+            alignmentDirection /= alignmentCount;
+
+        if (cohesionCount > 0)
+            cohesionDirection /= cohesionCount;
+
+        //get direction to center of mass
+        cohesionDirection -= transform.position;
+
+        //weighted rules
+        steering += separationDirection.normalized * separationWeight;
+        steering += alignmentDirection.normalized * alignmentWeight;
+        steering += cohesionDirection.normalized * cohesionWeight;
+
+        //local leader
+        if (leaderBoid != null)
+            steering += (leaderBoid.transform.position - transform.position).normalized;
+
+        //target finding
+        if (target != null)
+        {
+            follow -= transform.position;
+            steering += follow.normalized;
+        }
+            
+        //obstacle avoidance
+        if (Physics.Raycast(transform.position, transform.forward, out var hitInfo, localAreaRadius, LayerMask.GetMask("Default")))
             steering = ((hitInfo.point + hitInfo.normal) - transform.position).normalized;
 
         //apply steering
--- Assets/Scripts/BoidSpawner.cs
+++ Assets/Scripts/BoidSpawner.cs
@@ -3,8 +3,17 @@ using UnityEngine;
 
 public class BoidSpawner : MonoBehaviour
 {
+    [Header("Prefabs")]
     [SerializeField]
-    private BoidController boidPrefab;
+    private GameObject waspPrefab;
+    [SerializeField]
+    private GameObject beetlePrefab;
+    [SerializeField]
+    private GameObject scorpionPrefab;
+
+    [Header("Target")] 
+    [SerializeField] 
+    private GameObject target;
     
     [Header("Spawner Variables")]
     [SerializeField]
@@ -12,23 +21,103 @@ public class BoidSpawner : MonoBehaviour
     [SerializeField]
     private float boidSimulationArea = 50f;
 
-    private List<BoidController> boids;
+    [Header("Flocking Controls")] 
+    [SerializeField, Range(0f, 10f)]
+    private float separationWeight;
+    [SerializeField, Range(0f, 10f)]
+    private float alignmentWeight;
+    [SerializeField, Range(0f, 10f)]
+    private float cohesionWeight;
+    
+    [Header("Default Flocking Variables")]
+    // Separation
+    [SerializeField] private float noClumpingRadius = 100f;
+    // Alignment and Cohesion
+    [SerializeField] private float localAreaRadius = 10f;
+    // Speed
+    [SerializeField] private float speed = 10f;
+    [SerializeField] private float steeringSpeed = 100f;
+    
+    [SerializeField]
+    private List<BoidController> wasps;
+    [SerializeField]
+    private List<BoidController> beetles;
+    [SerializeField]
+    private List<BoidController> scorpions;
 
     private void Start()
     {
-        boids = new List<BoidController>();
+        wasps = new List<BoidController>();
+        beetles = new List<BoidController>();
+        scorpions = new List<BoidController>();
 
-        for (var i = 0; i < spawnBoids; i++)
+        for (var i = 0; i < spawnBoids/3; i++)
         {
-            SpawnBoid(boidPrefab.gameObject, 0);
+            SpawnBoid(waspPrefab, 0, wasps);
+        }
+        for (var i = 0; i < spawnBoids/3; i++)
+        {
+            SpawnBoid(beetlePrefab, 1, beetles);
+        }
+        for (var i = 0; i < spawnBoids/3; i++)
+        {
+            SpawnBoid(scorpionPrefab, 2, scorpions);
         }
     }
 
     private void Update()
     {
-        foreach (var boid in boids)
+        foreach (var boid in wasps)
         {
-            boid.SimulateMovement(boids, Time.deltaTime);
+            boid.GetComponent<BoidController>().SimulateMovement(wasps, Time.deltaTime, separationWeight, alignmentWeight, cohesionWeight);
+
+            var boidPos = boid.transform.position;
+
+            if (boidPos.x > boidSimulationArea)
+                boidPos.x -= boidSimulationArea * 2;
+            else if (boidPos.x < -boidSimulationArea)
+                boidPos.x += boidSimulationArea * 2;
+
+            if (boidPos.y > boidSimulationArea)
+                boidPos.y -= boidSimulationArea * 2;
+            else if (boidPos.y < -boidSimulationArea)
+                boidPos.y += boidSimulationArea * 2;
+
+            if (boidPos.z > boidSimulationArea)
+                boidPos.z -= boidSimulationArea * 2;
+            else if (boidPos.z < -boidSimulationArea)
+                boidPos.z += boidSimulationArea * 2;
+
+            boid.transform.position = boidPos;
+        }
+        
+        foreach (var boid in beetles)
+        {
+            boid.GetComponent<BoidController>().SimulateMovement(beetles, Time.deltaTime, separationWeight, alignmentWeight, cohesionWeight);
+
+            var boidPos = boid.transform.position;
+
+            if (boidPos.x > boidSimulationArea)
+                boidPos.x -= boidSimulationArea * 2;
+            else if (boidPos.x < -boidSimulationArea)
+                boidPos.x += boidSimulationArea * 2;
+
+            if (boidPos.y > boidSimulationArea)
+                boidPos.y -= boidSimulationArea * 2;
+            else if (boidPos.y < -boidSimulationArea)
+                boidPos.y += boidSimulationArea * 2;
+
+            if (boidPos.z > boidSimulationArea)
+                boidPos.z -= boidSimulationArea * 2;
+            else if (boidPos.z < -boidSimulationArea)
+                boidPos.z += boidSimulationArea * 2;
+
+            boid.transform.position = boidPos;
+        }
+        
+        foreach (var boid in scorpions)
+        {
+            boid.GetComponent<BoidController>().SimulateMovement(scorpions, Time.deltaTime, separationWeight, alignmentWeight, cohesionWeight);
 
             var boidPos = boid.transform.position;
 
@@ -51,16 +140,21 @@ public class BoidSpawner : MonoBehaviour
         }
     }
 
-    private void SpawnBoid(GameObject prefab, int swarmIndex)
+    private void SpawnBoid(GameObject prefab, int swarmIndex, List<BoidController> list)
     {
-        var boidInstance = Instantiate(prefab, this.transform);
-
+        var boidInstance = Instantiate(prefab, transform);
+        boidInstance.GetComponent<BoidController>().SetFlockingVariables(noClumpingRadius, localAreaRadius, speed, steeringSpeed, target);
         boidInstance.transform.localPosition += new Vector3(Random.Range(-10, 10), Random.Range(-10, 10), Random.Range(-10, 10));
         boidInstance.transform.localRotation = Quaternion.Euler(Random.Range(0, 360), Random.Range(0, 360), Random.Range(0, 360));
 
         var boidController = boidInstance.GetComponent<BoidController>();
         boidController.SwarmIndex = swarmIndex;
 
-        boids.Add(boidController);
+        list.Add(boidController);
+    }
+    
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireCube(transform.position, new Vector3(boidSimulationArea * 2, boidSimulationArea * 2, boidSimulationArea * 2));
     }
 }
